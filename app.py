a596eb64d6c8258585766dee89e14c04a90122ca
Maman08 <a34mritunjaysingh@gmail.com>
2025-07-01 06:56:39 +0530

    Initial commit

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return jsonify({'msg: Hello from version 1 !'})
+    return jsonify({'message': 'Hello from version 1!'})
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+    
