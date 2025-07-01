fdceac6b9547d5698e508b437463b929d6b1da88
Maman08 <a34mritunjaysingh@gmail.com>
2025-07-01 06:41:14 +0530

    Initial commit

--- app.py
+++ app.py
@@ -1,13 +1,9 @@
-from flask import Flask ,render_template
+from flask import Flask ,jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
-@app.route('/health')
-def health():
-    return "OK", 200
-
+    return jsonify({'msg: Hello from version 1 !'})
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
